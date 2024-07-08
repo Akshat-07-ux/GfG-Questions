8a03a58d830d294565b1f334d73696f057f41a3a
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2024-07-09 03:29:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+   public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+        // Sort arrival and departure arrays
+        sort(arr, arr + n);
+        sort(dep, dep + n);
+        
+        int platformsNeeded = 1;
+        int result = 1;
+        int i = 1, j = 0;
+        
+        while (i < n && j < n) {
+            // If next event is arrival, increment count of platforms needed
+            if (arr[i] <= dep[j]) {
+                platformsNeeded++;
+                i++;
+            }
+            // If next event is departure, decrement count of platforms needed
+            else if (arr[i] > dep[j]) {
+                platformsNeeded--;
+                j++;
+            }
+            
+            // Update result if needed
+            if (platformsNeeded > result) {
+                result = platformsNeeded;
+            }
+        }
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}
+// } Driver Code Ends
