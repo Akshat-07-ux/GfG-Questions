7a66cb9da43583c5996abf10260d6dcfd80bbb0d
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2024-08-14 02:47:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+ public:
+    int longestCommonSubstr(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+        
+        // Create a 2D array to store the length of longest common suffixes
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        
+        int result = 0; // To store length of the longest common substring
+        
+        // Build dp table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    result = max(result, dp[i][j]);
+                }
+                else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr(s1, s2) << endl;
+    }
+}
+// } Driver Code Ends
