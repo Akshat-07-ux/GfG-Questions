a742bf848abd6f2923e1c113c2fe85e0f25a33c5
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2024-08-30 20:48:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Index/maximum-index.cpp
+++ Difficulty: Medium/Maximum Index/maximum-index.cpp
@@ -1,66 +1,59 @@
 //{ Driver Code Starts
 #include <bits/stdc++.h>
+
 using namespace std;
 
-
 // } Driver Code Ends
-class Solution {
+class Solution{
 public:
-    // Function to find the maximum index difference.
-    int maxIndexDiff(std::vector<int>& arr) {
-        int n = arr.size();
-        if (n <= 1) return 0;
-
-        // Create and fill leftMin array
-        std::vector<int> leftMin(n);
-        leftMin[0] = arr[0];
+    int maxIndexDiff(int arr[], int n) {
+        // Create arrays to store minimum values from the start and maximum values from the end
+        int LMin[n], RMax[n];
+        
+        // Fill LMin array
+        LMin[0] = arr[0];
         for (int i = 1; i < n; i++) {
-            leftMin[i] = std::min(arr[i], leftMin[i - 1]);
+            LMin[i] = std::min(LMin[i - 1], arr[i]);
         }
-
-        // Create and fill rightMax array
-        std::vector<int> rightMax(n);
-        rightMax[n - 1] = arr[n - 1];
-        for (int j = n - 2; j >= 0; j--) {
-            rightMax[j] = std::max(arr[j], rightMax[j + 1]);
+        
+        // Fill RMax array
+        RMax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            RMax[i] = std::max(RMax[i + 1], arr[i]);
         }
-
-        // Traverse both arrays to find the maximum j - i
+        
+        // Traverse LMin and RMax to find the maximum difference
         int i = 0, j = 0, maxDiff = 0;
-        while (j < n && i < n) {
-            if (leftMin[i] <= rightMax[j]) {
+        while (j < n) {
+            if (LMin[i] <= RMax[j]) {
                 maxDiff = std::max(maxDiff, j - i);
                 j++;
             } else {
                 i++;
             }
         }
-
+        
         return maxDiff;
     }
 };
 
+
 //{ Driver Code Starts.
 
 int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
+    int t;
+    cin >> t;
     while (t--) {
-        string line;
-        getline(cin, line);
-        stringstream ss(line);
-        vector<int> nums;
-        int num;
-        while (ss >> num) {
-            nums.push_back(num);
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
         }
         Solution ob;
-
-        // calling maxIndexDiff() function
-        cout << ob.maxIndexDiff(nums) << endl;
+        auto ans = ob.maxIndexDiff(a, n);
+        cout << ans << "\n";
     }
-
     return 0;
 }
 // } Driver Code Ends
