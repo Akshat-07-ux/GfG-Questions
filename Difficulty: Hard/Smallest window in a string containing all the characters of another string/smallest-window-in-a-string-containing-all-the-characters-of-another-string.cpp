5a4a99b4dcc15db99cd3da0fb54d9421a4dcd363
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2024-09-24 11:35:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window in a string containing all the characters of another string/smallest-window-in-a-string-containing-all-the-characters-of-another-string.cpp
+++ Difficulty: Hard/Smallest window in a string containing all the characters of another string/smallest-window-in-a-string-containing-all-the-characters-of-another-string.cpp
@@ -6,58 +6,43 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
-  public:
-    // Function to find the smallest window in string s consisting of all characters of string p.
+   public:
     string smallestWindow(string s, string p) {
-        int m = s.length();
-        int n = p.length();
-        
-        if (m < n) return "-1"; // If s is smaller than p, no valid window exists.
-        
-        // Frequency map for string p
-        vector<int> pFreq(26, 0);
-        for (char ch : p) pFreq[ch - 'a']++;
-        
-        // Sliding window frequency map for string s
-        vector<int> sFreq(26, 0);
-        
-        int minWindowLen = INT_MAX;
-        int start = 0, startIdx = -1;
-        int count = 0; // To track how many characters are fully matched
-        
-        for (int end = 0; end < m; end++) {
-            // Include current character in the window
-            char endChar = s[end];
-            sFreq[endChar - 'a']++;
-            
-            // If the current character's frequency is less than or equal to the required frequency
-            if (pFreq[endChar - 'a'] != 0 && sFreq[endChar - 'a'] <= pFreq[endChar - 'a']) {
+        vector<int> freq_p(256, 0), freq_window(256, 0);
+        int start = 0, min_length = INT_MAX, count = 0;
+        string result = "-1";
+
+        // Count frequency of characters in p
+        for (char c : p) {
+            freq_p[c]++;
+        }
+
+        for (int end = 0; end < s.length(); end++) {
+            // Process current character
+            if (freq_p[s[end]] > 0 && ++freq_window[s[end]] <= freq_p[s[end]]) {
                 count++;
             }
-            
-            // Once we have matched all characters
-            while (count == n) {
-                // Update minimum window if the current window is smaller
-                if (end - start + 1 < minWindowLen) {
-                    minWindowLen = end - start + 1;
-                    startIdx = start;
+
+            // If all characters are matched
+            if (count == p.length()) {
+                // Try to minimize the window
+                while (freq_p[s[start]] == 0 || freq_window[s[start]] > freq_p[s[start]]) {
+                    if (freq_window[s[start]] > freq_p[s[start]]) {
+                        freq_window[s[start]]--;
+                    }
+                    start++;
                 }
-                
-                // Try to shrink the window from the left
-                char startChar = s[start];
-                sFreq[startChar - 'a']--;
-                
-                // If the character we are removing is part of p and now its frequency is below requirement
-                if (pFreq[startChar - 'a'] != 0 && sFreq[startChar - 'a'] < pFreq[startChar - 'a']) {
-                    count--;
+
+                // Update result if current window is smaller
+                int window_length = end - start + 1;
+                if (window_length < min_length) {
+                    min_length = window_length;
+                    result = s.substr(start, min_length);
                 }
-                
-                start++; // Move the start pointer to shrink the window
             }
         }
-        
-        // If startIdx was updated, return the substring, otherwise return "-1"
-        return startIdx == -1 ? "-1" : s.substr(startIdx, minWindowLen);
+
+        return result;
     }
 };
 
