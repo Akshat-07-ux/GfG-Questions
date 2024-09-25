3ec78c787aa63ffc4e7cee550df02ad7a58e97bf
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2024-09-26 00:43:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Hello World/java-hello-world.java
@@ -0,0 +1,26 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Solution ob = new Solution();
+        ob.printHelloWorld();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+       static void printHelloWorld() {
+        // Print "Hello World" on a single line
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        // Calling the function to print the output
+        printHelloWorld();
+    }
+}
