0e50ff5243ed91603081d80866d934b1ff148822
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2024-09-27 21:55:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check if Tree is Isomorphic/check-if-tree-is-isomorphic.cpp
@@ -0,0 +1,144 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+Node is as follows:
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+ public:
+    // Function to check if two trees are isomorphic.
+    bool isIsomorphic(Node *root1, Node *root2) {
+        // Base cases
+        if (root1 == NULL && root2 == NULL) {
+            return true;  // Both trees are empty, they are isomorphic
+        }
+        if (root1 == NULL || root2 == NULL) {
+            return false; // One tree is empty and the other is not
+        }
+        if (root1->data != root2->data) {
+            return false; // Data mismatch at the current nodes
+        }
+        
+        // There are two cases for the trees to be isomorphic:
+        // 1. Without flipping
+        // 2. With flipping
+        bool withoutFlip = isIsomorphic(root1->left, root2->left) && 
+                           isIsomorphic(root1->right, root2->right);
+        
+        bool withFlip = isIsomorphic(root1->left, root2->right) && 
+                        isIsomorphic(root1->right, root2->left);
+        
+        // If either without flip or with flip holds true, the trees are isomorphic
+        return withoutFlip || withFlip;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s1,s2;
+		getline(cin,s1);
+		getline(cin,s2);
+		Node* root1 = buildTree(s1);
+		Node* root2 = buildTree(s2);
+		Solution obj;
+		if(obj.isIsomorphic(root1,root2))
+		    cout<<"Yes"<<endl;
+		else
+		    cout<<"No"<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
