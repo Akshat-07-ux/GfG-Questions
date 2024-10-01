507ecc3b6f49d6a057ff15370a91f31bc4ee812e
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2024-10-01 22:29:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count of smaller elements/count-of-smaller-elements.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+ public:
+    int countOfElements(int x, vector<int>& arr) {
+        int count = 0;
+
+        // Traverse the array and count elements <= x
+        for (int num : arr) {
+            if (num <= x) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int x;
+        scanf("%d ", &x);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.countOfElements(x, arr);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
