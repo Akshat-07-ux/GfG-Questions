2fa616d08c21ef4b4335fac5ea1385c2a5613e10
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2024-11-30 19:45:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.cpp
+++ Difficulty: Easy/Anagram/anagram.cpp
@@ -4,36 +4,31 @@ using namespace std;
 
 
 // } Driver Code Ends
-class Solution
-{
-    public:
-    // Function is to check whether two strings are anagram of each other or not.
-    bool isAnagram(string a, string b) {
-        // Step 1: Check if lengths are different
-        if (a.length() != b.length()) {
+class Solution {
+   public:
+    // Function to check whether two strings are anagrams of each other or not.
+    bool areAnagrams(string& s1, string& s2) {
+        // If the lengths are different, they cannot be anagrams.
+        if (s1.size() != s2.size()) {
             return false;
         }
-
-        // Step 2: Create frequency arrays for both strings
-        vector<int> count(26, 0); // Since there are 26 lowercase English letters
-
-        // Count characters for string a
-        for (char ch : a) {
-            count[ch - 'a']++;
+        
+        // Create a frequency map for the characters in s1.
+        unordered_map<char, int> freq;
+        
+        for (char c : s1) {
+            freq[c]++;
         }
-
-        // Subtract characters for string b
-        for (char ch : b) {
-            count[ch - 'a']--;
-        }
-
-        // Check if all counts are zero
-        for (int i : count) {
-            if (i != 0) {
-                return false;
+        
+        // Decrease the frequency for characters in s2.
+        for (char c : s2) {
+            if (freq.find(c) == freq.end() || freq[c] == 0) {
+                return false; // Character in s2 not found in s1 or frequency mismatch.
             }
+            freq[c]--;
         }
-
+        
+        // If all frequencies are zero, the strings are anagrams.
         return true;
     }
 };
@@ -41,20 +36,22 @@ class Solution
 //{ Driver Code Starts.
 
 int main() {
-    
+
     int t;
 
     cin >> t;
 
-    while(t--){
+    while (t--) {
         string c, d;
 
         cin >> c >> d;
         Solution obj;
-        if(obj.isAnagram(c, d)) cout << "YES" << endl;
-        else cout << "NO" << endl;
+        if (obj.areAnagrams(c, d))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
     }
-
 }
 
 // } Driver Code Ends
