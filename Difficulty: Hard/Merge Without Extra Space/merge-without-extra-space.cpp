9d5ad3ab6d8eaf315c29c2cd345554aaf7bb7044
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2024-12-11 22:19:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Merge Without Extra Space/merge-without-extra-space.cpp
+++ Difficulty: Hard/Merge Without Extra Space/merge-without-extra-space.cpp
@@ -1,85 +1,76 @@
 //{ Driver Code Starts
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
 
 // } Driver Code Ends
-class Solution{
-
+class Solution {
 public:
-    // Function to merge the arrays.
-    void merge(long long arr1[], long long arr2[], int n, int m) 
-    { 
-        int gap = n + m;
-        for (gap = nextGap(gap); gap > 0; gap = nextGap(gap)) {
-            // comparing elements in the first array
-            int i, j;
-            for (i = 0; i + gap < n; i++) {
-                if (arr1[i] > arr1[i + gap]) {
-                    std::swap(arr1[i], arr1[i + gap]);
-                }
-            }
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        int n = a.size();
+        int m = b.size();
 
-            // comparing elements in both arrays
-            for (j = (gap > n) ? gap - n : 0; i < n && j < m; i++, j++) {
-                if (arr1[i] > arr2[j]) {
-                    std::swap(arr1[i], arr2[j]);
-                }
-            }
-
-            // comparing elements in the second array
-            if (j < m) {
-                for (j = 0; j + gap < m; j++) {
-                    if (arr2[j] > arr2[j + gap]) {
-                        std::swap(arr2[j], arr2[j + gap]);
-                    }
-                }
+        // Start comparing elements from the end of `a` and the start of `b`
+        for (int i = n - 1, j = 0; i >= 0 && j < m; i--, j++) {
+            // Swap if the element in `a` is greater than the element in `b`
+            if (a[i] > b[j]) {
+                swap(a[i], b[j]);
+            } else {
+                break;
             }
         }
-    }
-    
-    // Function to calculate the next gap
-    int nextGap(int gap) {
-        if (gap <= 1) return 0;
-        return (gap / 2) + (gap % 2);
+
+        // Sort both arrays to maintain sorted order
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
     }
 };
 
+
 //{ Driver Code Starts.
 
-int main() 
-{ 
-	
-	int T;
-	cin >> T;
-	
-	while(T--){
-	    int n, m;
-	    cin >> n >> m;
-	    
-	    long long arr1[n], arr2[m];
-	    
-	    for(int i = 0;i<n;i++){
-	        cin >> arr1[i];
-	    }
-	    
-	    for(int i = 0;i<m;i++){
-	        cin >> arr2[i];
-	    }
-	    Solution ob;
-	    ob.merge(arr1, arr2, n, m); 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-        for (int i = 0; i < n; i++) 
-            cout<<arr1[i]<<" "; 
-        
-       
-	    for (int i = 0; i < m; i++) 
-		    cout<<arr2[i]<<" "; 
-	    
-	    cout<<endl;
-	}
+    int t;
+    cin >> t; // Inputting the test cases
 
-	return 0; 
-} 
+    while (t--) {
+        vector<int> a, b;
+
+        // Reading the first array as a space-separated line
+        string arr1;
+        getline(cin >> ws, arr1); // Use ws to ignore any leading whitespace
+        stringstream ss1(arr1);
+        int num;
+        while (ss1 >> num) {
+            a.push_back(num);
+        }
+
+        // Reading the second array as a space-separated line
+        string arr2;
+        getline(cin, arr2);
+        stringstream ss2(arr2);
+        while (ss2 >> num) {
+            b.push_back(num);
+        }
+
+        Solution ob;
+        ob.mergeArrays(a, b);
+
+        // Output the merged result
+        for (int i = 0; i < a.size(); i++) {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+        for (int i = 0; i < b.size(); i++) {
+            cout << b[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
 
 // } Driver Code Ends
