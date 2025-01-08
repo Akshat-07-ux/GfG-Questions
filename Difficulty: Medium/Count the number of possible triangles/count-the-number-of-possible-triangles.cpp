b128d775fc402d9e148a66a3efd92b9fbc17b72c
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-01-08 16:30:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
@@ -4,55 +4,58 @@ using namespace std;
 
 
 // } Driver Code Ends
-class Solution
-{
+class Solution {
 public:
     // Function to count the number of possible triangles.
-    static int findNumberOfTriangles(int arr[], int n)
-    {
-        // Sort the array
-        sort(arr, arr + n);
-        
+    int countTriangles(vector<int>& arr) {
+        int n = arr.size();
+        if (n < 3) return 0;
+
+        sort(arr.begin(), arr.end());  // Sort the array
         int count = 0;
-        
-        // Fix the third side (largest side in the triangle)
+
+        // Fix the third side (largest side) and use two pointers for the other two sides
         for (int k = n - 1; k >= 2; k--) {
             int i = 0, j = k - 1;
-            
-            // Use two pointers to find valid pairs
+
             while (i < j) {
-                // If the sum of arr[i] and arr[j] is greater than arr[k],
-                // then all pairs (arr[i], arr[i+1], ..., arr[j]) will form a valid triangle
+                // Check if the sum of two smaller sides is greater than the largest side
                 if (arr[i] + arr[j] > arr[k]) {
-                    count += (j - i);
-                    j--;
+                    count += (j - i);  // All pairs between i and j are valid
+                    j--;  // Move the second pointer to the left
                 } else {
-                    i++;
+                    i++;  // Move the first pointer to the right
                 }
             }
         }
-        
+
         return count;
     }
 };
 
-
 //{ Driver Code Starts.
 
-int main()
-{
-    int T;
-    cin>>T;
-    while(T--)
-    {
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To ignore the newline after the integer input
+    while (t--) {
         int n;
-        cin>>n;
-        int arr[n];
-        for(int i=0; i<n; i++)
-            cin>>arr[i];
-        Solution ob;
-        cout<<ob.findNumberOfTriangles(arr,n) <<endl;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.countTriangles(a) << "\n~\n";
     }
+
     return 0;
 }
+
 // } Driver Code Ends
