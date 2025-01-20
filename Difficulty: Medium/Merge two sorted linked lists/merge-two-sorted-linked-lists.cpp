581f7839ffac18ba2722864b561652fd651b778b
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-01-20 15:48:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
@@ -18,13 +18,27 @@ void printList(struct Node *head) {
         cout << temp->data << ' ';
         temp = temp->next;
     }
-    cout << '\n';
+    cout << "\n~\n";
+}
+
+Node *insertSorted(Node *head, int data) {
+    Node *new_node = new Node(data);
+    if (!head || head->data >= data) {
+        new_node->next = head;
+        return new_node;
+    }
+
+    Node *current = head;
+    while (current->next && current->next->data < data) {
+        current = current->next;
+    }
+    new_node->next = current->next;
+    current->next = new_node;
+    return head;
 }
 
 
 // } Driver Code Ends
-
-
 /* Link list Node
 struct Node {
   int data;
@@ -36,44 +50,44 @@ struct Node {
   }
 };
 */
-// Function to merge two sorted linked list.
 class Solution {
-public:
-    // Function to merge two sorted linked lists.
+ public:
     Node* sortedMerge(Node* head1, Node* head2) {
-        // If one of the lists is empty, return the other list.
+        // If either of the lists is empty, return the other list
         if (!head1) return head2;
         if (!head2) return head1;
-        
-        // Initialize a dummy node to help in merging lists.
-        Node* dummy = new Node(0); // A dummy node to hold the merged list
-        Node* tail = dummy;        // Pointer to build the merged list
-        
-        // Traverse both lists until one of them becomes empty.
+
+        // Pointer to the merged list's head
+        Node* mergedHead = NULL;
+
+        // Select the smallest node between head1 and head2
+        if (head1->data <= head2->data) {
+            mergedHead = head1;
+            head1 = head1->next;
+        } else {
+            mergedHead = head2;
+            head2 = head2->next;
+        }
+
+        // Pointer to track the current end of the merged list
+        Node* current = mergedHead;
+
+        // Traverse both lists and merge them
         while (head1 && head2) {
             if (head1->data <= head2->data) {
-                // If the data in the first list's node is smaller or equal, append it to the result.
-                tail->next = head1;
+                current->next = head1;
                 head1 = head1->next;
             } else {
-                // Otherwise, append the node from the second list to the result.
-                tail->next = head2;
+                current->next = head2;
                 head2 = head2->next;
             }
-            // Move the tail pointer forward.
-            tail = tail->next;
+            current = current->next;
         }
-        
-        // Append the remaining nodes from whichever list is non-empty.
-        if (head1) {
-            tail->next = head1;
-        } else {
-            tail->next = head2;
-        }
-        
-        // The merged list is pointed by dummy->next.
-        Node* mergedHead = dummy->next;
-        delete dummy; // Free the memory allocated for the dummy node
+
+        // Attach the remaining nodes of either list
+        if (head1) current->next = head1;
+        if (head2) current->next = head2;
+
         return mergedHead;
     }
 };
@@ -87,34 +101,19 @@ int main() {
     cin.ignore();
     while (T--) {
         int n1, n2, tmp;
-        Node *head1 = nullptr, *tail1 = nullptr;
-        Node *head2 = nullptr, *tail2 = nullptr;
+        Node *head1 = nullptr, *head2 = nullptr;
         string input1, input2;
 
         getline(cin, input1); // Read the entire line for the LL1 elements
         stringstream ss1(input1);
         while (ss1 >> tmp) {
-            Node *new_node = new Node(tmp);
-            if (head1 == nullptr) {
-                head1 = new_node;
-                tail1 = new_node;
-            } else {
-                tail1->next = new_node;
-                tail1 = new_node;
-            }
+            head1 = insertSorted(head1, tmp);
         }
 
         getline(cin, input2); // Read the entire line for the LL2 elements
         stringstream ss2(input2);
         while (ss2 >> tmp) {
-            Node *new_node = new Node(tmp);
-            if (head2 == nullptr) {
-                head2 = new_node;
-                tail2 = new_node;
-            } else {
-                tail2->next = new_node;
-                tail2 = new_node;
-            }
+            head2 = insertSorted(head2, tmp);
         }
 
         Solution obj;
