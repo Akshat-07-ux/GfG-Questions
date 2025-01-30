074f718b79c1d78323f77141f96b6da28ea44534
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-01-30 16:28:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -4,67 +4,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-    vector<vector<int>> nQueen(int n) {
-        vector<vector<int>> solutions;
-        vector<int> board(n, 0); // board[i] will represent the row position of the queen in the i-th column
-        solveNQueens(n, 0, board, solutions);
-        return solutions;
-    }
-    
-private:
-    void solveNQueens(int n, int col, vector<int>& board, vector<vector<int>>& solutions) {
-        if (col == n) {
-            solutions.push_back(board);
-            return;
-        }
-        
-        for (int row = 1; row <= n; ++row) {
-            if (isSafe(row, col, board)) {
-                board[col] = row;
-                solveNQueens(n, col + 1, board, solutions);
-                board[col] = 0; // backtrack
-            }
-        }
-    }
-    
-    bool isSafe(int row, int col, const vector<int>& board) {
-        for (int i = 0; i < col; ++i) {
-            if (board[i] == row || // Check if a queen is in the same row
-                abs(board[i] - row) == abs(i - col)) { // Check if a queen is on the same diagonal
+class Solution {
+  public:
+    vector<vector<int>> result;
+
+    bool isSafe(vector<int>& board, int row, int col, int n) {
+        for (int prevCol = 0; prevCol < col; prevCol++) {
+            int prevRow = board[prevCol];
+            if (prevRow == row || abs(prevRow - row) == abs(prevCol - col)) {
                 return false;
             }
         }
         return true;
     }
+
+    void solve(int col, vector<int>& board, int n) {
+        if (col == n) {
+            result.push_back(board);
+            return;
+        }
+
+        for (int row = 1; row <= n; row++) {
+            if (isSafe(board, row, col, n)) {
+                board[col] = row;
+                solve(col + 1, board, n);
+            }
+        }
+    }
+
+    vector<vector<int>> nQueen(int n) {
+        vector<int> board(n, 0);
+        solve(0, board, n);
+        return result;
+    }
 };
+
 
 //{ Driver Code Starts.
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        
+        cin >> n;
+
         Solution ob;
         vector<vector<int>> ans = ob.nQueen(n);
-        if(ans.size() == 0)
-            cout<<-1<<"\n";
+        if (ans.size() == 0)
+            cout << -1 << "\n";
         else {
-            for(int i = 0;i < ans.size();i++){
-                cout<<"[";
-                for(int u: ans[i])
-                    cout<<u<<" ";
-                cout<<"] ";
+            sort(ans.begin(), ans.end());
+            for (int i = 0; i < ans.size(); i++) {
+                cout << "[";
+                for (int u : ans[i])
+                    cout << u << " ";
+                cout << "] ";
             }
-            cout<<endl;
+            cout << endl;
         }
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
