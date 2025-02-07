bb929c2c5c8b2e515d52e2c086bec07a9c10b23d
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-02-07 19:48:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of dependencies in a graph/sum-of-dependencies-in-a-graph.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution {
+   public:
+    // Function to calculate the sum of dependencies.
+    int sumOfDependencies(int V, vector<vector<int>> &edges) {
+        int sum = 0;
+        
+        // Iterate through the edges list and count dependencies
+        for (auto &edge : edges) {
+            sum++; // Since each directed edge (u, v) adds a dependency for u
+        }
+        
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M, x, y;
+        cin >> N >> M;
+        vector<vector<int>> edges(M, vector<int>(2));
+        for (int i = 0; i < M; i++) {
+            cin >> x >> y;
+            edges[i][0] = x;
+            edges[i][1] = y;
+        }
+        Solution ob;
+        cout << ob.sumOfDependencies(N, edges) << "\n";
+        cout << "~" << endl;
+    }
+}
+// } Driver Code Ends
