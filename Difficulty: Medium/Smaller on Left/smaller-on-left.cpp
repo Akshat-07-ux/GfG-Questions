da7a6f9bf544cee7f4ae81d1dfa4db5d66254756
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-02-21 20:34:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smaller on Left/smaller-on-left.cpp
+++ Difficulty: Medium/Smaller on Left/smaller-on-left.cpp
@@ -1,59 +1,56 @@
 //{ Driver Code Starts
-// Initial Template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-// User function Template for C++
+vector<int> Smallestonleft(int arr[], int n);
 
-class Solution{
-public:
-    vector<int> leftSmaller(int n, int a[]) {
-        vector<int> result(n);  // To store the result
-        stack<int> st;  // Stack to keep track of previous elements
+int main() {
 
-        for (int i = 0; i < n; ++i) {
-            // Pop elements from the stack until we find a smaller element
-            while (!st.empty() && st.top() >= a[i]) {
-                st.pop();
-            }
+    int t;
 
-            // If stack is empty, no smaller element on the left
-            if (st.empty()) {
-                result[i] = -1;
-            } else {
-                result[i] = st.top();
-            }
+    cin >> t;
 
-            // Push the current element onto the stack
-            st.push(a[i]);
+    while (t--) {
+
+        int n;
+        cin >> n;
+        int arr[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
         }
 
-        return result;
-    }
-};
+        vector<int> b;
+        b = Smallestonleft(arr, n);
+        for (int i = 0; i < n; i++)
+            cout << b[i] << " ";
 
-//{ Driver Code Starts.
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int a[n];
-        for(int i = 0;i < n;i++)
-            cin>>a[i];
-        
-        Solution ob;
-        vector<int> ans = ob.leftSmaller(n, a);
-        for(int i = 0;i < n;i++)
-            cout<<ans[i]<<" ";
-        cout<<endl;
+        cout << endl;
     
 cout << "~" << "\n";
 }
+
     return 0;
 }
 // } Driver Code Ends
+
+
+vector<int> Smallestonleft(int arr[], int n) {
+    vector<int> result;
+    set<int> bst; // Using ordered set as a self-balancing BST
+
+    for (int i = 0; i < n; i++) {
+        auto it = bst.lower_bound(arr[i]); // Find first element >= arr[i]
+        
+        if (it == bst.begin()) {
+            result.push_back(-1); // No smaller element found
+        } else {
+            --it; // Move to the greatest element smaller than arr[i]
+            result.push_back(*it);
+        }
+        
+        bst.insert(arr[i]); // Insert current element into BST
+    }
+    
+    return result;
+}
