3cb198d8a38210348cd0e6dbf403ebaa419126cb
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-02-24 19:48:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.cpp
+++ Difficulty: Medium/Stock span problem/stock-span-problem.cpp
@@ -1,74 +1,52 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-
-
-class Solution{
- public:
-    //Function to calculate the span of stock’s price for all n days.
-    vector<int> calculateSpan(int price[], int n)
-    {
-        // Result vector to store the span of each day.
+class Solution {
+public:
+    vector<int> calculateSpan(vector<int>& arr) {
+        int n = arr.size();
         vector<int> span(n);
-        
-        // Stack to store indices of the price array.
-        stack<int> s;
-        
-        // Iterate over all days.
+        stack<int> st;
+
         for (int i = 0; i < n; i++) {
-            // Pop elements from the stack while the stack is not empty and the
-            // current price is greater than or equal to the price at the top of the stack.
-            while (!s.empty() && price[i] >= price[s.top()]) {
-                s.pop();
+            while (!st.empty() && arr[st.top()] <= arr[i]) {
+                st.pop();
             }
-            
-            // If stack becomes empty, it means the current price is greater than all previous prices.
-            // So the span is i+1.
-            if (s.empty()) {
-                span[i] = i + 1;
-            }
-            else {
-                // Otherwise, the span is the difference between current index and the last higher price's index.
-                span[i] = i - s.top();
-            }
-            
-            // Push the current index to the stack.
-            s.push(i);
+
+            span[i] = (st.empty()) ? (i + 1) : (i - st.top());
+            st.push(i);
         }
         
         return span;
     }
 };
 
-
 //{ Driver Code Starts.
 
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cin>>n;
-		int i,a[n];
-		for(i=0;i<n;i++)
-		{
-			cin>>a[i];
-		}
-		Solution obj;
-		vector <int> s = obj.calculateSpan(a, n);
-		
-		for(i=0;i<n;i++)
-		{
-			cout<<s[i]<<" ";
-		}
-		cout<<endl;
-	}
-	return 0;
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.calculateSpan(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
 }
-
 // } Driver Code Ends
