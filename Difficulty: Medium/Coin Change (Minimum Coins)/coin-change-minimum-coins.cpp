6caa48cc5a85d635aa29598a82188aec7bfea182
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-03-15 20:44:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Minimum Coins)/coin-change-minimum-coins.cpp
+++ Difficulty: Medium/Coin Change (Minimum Coins)/coin-change-minimum-coins.cpp
@@ -4,42 +4,48 @@ using namespace std;
 
 
 // } Driver Code Ends
-class Solution {
 
-  public:
+class Solution {
+public:
     int minCoins(vector<int> &coins, int sum) {
-        int n = coins.size();
         vector<int> dp(sum + 1, INT_MAX);
-        dp[0] = 0; // base case: 0 coins needed to make sum 0
-        
-        // Fill the dp array
-        for (int coin : coins) {
-            for (int i = coin; i <= sum; i++) {
-                if (dp[i - coin] != INT_MAX) {
+        dp[0] = 0;
+
+        for (int i = 1; i <= sum; i++) {
+            for (int coin : coins) {
+                if (i >= coin && dp[i - coin] != INT_MAX) {
                     dp[i] = min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        
+
         return dp[sum] == INT_MAX ? -1 : dp[sum];
     }
 };
 
+
 //{ Driver Code Starts.
+
 int main() {
-
-    int t;
-    cin >> t;
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
     while (t--) {
-        int v, m;
-        cin >> v >> m;
-
-        vector<int> coins(m);
-        for (int i = 0; i < m; i++)
-            cin >> coins[i];
-
-        Solution ob;
-        cout << ob.minCoins(coins, v) << "\n";
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.minCoins(arr, k);
+        cout << res << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
