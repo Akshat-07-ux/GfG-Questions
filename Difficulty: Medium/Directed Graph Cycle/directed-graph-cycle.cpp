2f35e59e77dce16a36b4dd4ee0b8aed7e6245710
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-04-07 11:10:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -2,63 +2,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
+
 class Solution {
   public:
-    // Function to detect cycle in a directed graph.
-    bool isCyclic(int V, vector<int> adj[]) {
-        vector<bool> visited(V, false);
-        vector<bool> recStack(V, false);
-        
-        for(int i = 0; i < V; i++) {
-            if(dfs(i, adj, visited, recStack))
+    bool dfs(int node, vector<int> adj[], vector<bool> &visited, vector<bool> &recStack) {
+        visited[node] = true;
+        recStack[node] = true;
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                if (dfs(neighbor, adj, visited, recStack)) return true;
+            } else if (recStack[neighbor]) {
+                // cycle detected
                 return true;
+            }
         }
-        
+
+        recStack[node] = false;
         return false;
     }
 
-private:
-    bool dfs(int v, vector<int> adj[], vector<bool>& visited, vector<bool>& recStack) {
-        if(!visited[v]) {
-            visited[v] = true;
-            recStack[v] = true;
-            
-            for(int neighbor : adj[v]) {
-                if(!visited[neighbor] && dfs(neighbor, adj, visited, recStack))
-                    return true;
-                else if(recStack[neighbor])
-                    return true;
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        vector<int> adj[V];
+        for (auto &edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+        }
+
+        vector<bool> visited(V, false);
+        vector<bool> recStack(V, false);
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (dfs(i, adj, visited, recStack)) return true;
             }
         }
-        recStack[v] = false;
+
         return false;
     }
 };
 
+
 //{ Driver Code Starts.
 
 int main() {
-
-    int t;
-    cin >> t;
-    while (t--) {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
         int V, E;
         cin >> V >> E;
-
-        vector<int> adj[V];
-
-        for (int i = 0; i < E; i++) {
+        cin.ignore();
+        vector<vector<int>> edges;
+        for (int i = 1; i <= E; i++) {
             int u, v;
             cin >> u >> v;
-            adj[u].push_back(v);
+            edges.push_back({u, v});
         }
 
         Solution obj;
-        cout << obj.isCyclic(V, adj) << "\n";
+        bool ans = obj.isCyclic(V, edges);
+        if (ans)
+            cout << "true\n";
+        else
+            cout << "false\n";
     }
-
     return 0;
 }
-
 // } Driver Code Ends
