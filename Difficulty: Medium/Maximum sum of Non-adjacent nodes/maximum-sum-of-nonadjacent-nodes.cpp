6edc7adf302ac798706cd9f7b40c89e5acc63bcb
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-04-28 18:32:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum sum of Non-adjacent nodes/maximum-sum-of-nonadjacent-nodes.cpp
+++ Difficulty: Medium/Maximum sum of Non-adjacent nodes/maximum-sum-of-nonadjacent-nodes.cpp
@@ -1,97 +1,95 @@
 //{ Driver Code Starts
-//Initial Template for C++
+// Initial Template for C++
 
-#include<bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
 // Tree Node
-struct Node
-{
+struct Node {
     int data;
     Node* left;
     Node* right;
 };
 
 // Utility function to create a new Tree Node
-Node* newNode(int val)
-{
+Node* newNode(int val) {
     Node* temp = new Node;
     temp->data = val;
     temp->left = NULL;
     temp->right = NULL;
-    
+
     return temp;
 }
 
 // Function to Build Tree
-Node* buildTree(string str)
-{   
+Node* buildTree(string str) {
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
-    for(string str; iss >> str; )
+    for (string str; iss >> str;)
         ip.push_back(str);
-        
+
     // Create the root of the tree
     Node* root = newNode(stoi(ip[0]));
-        
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
+    while (!queue.empty() && i < ip.size()) {
+
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
-        if(i >= ip.size())
+        if (i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
 
 
 // } Driver Code Ends
-//User function Template for C++
 
-//Node Structure
+// User function Template for C++
+
+// Node Structure
 /*
 struct Node
 {
@@ -101,52 +99,51 @@ struct Node
 };
 */
 
-class Solution{
- public:
-    // Helper function to calculate maximum sum of non-adjacent nodes.
-    pair<int, int> helper(Node* root) {
-        if (!root) {
-            return {0, 0};
-        }
-
-        // Recur for left and right subtrees
-        pair<int, int> left = helper(root->left);
-        pair<int, int> right = helper(root->right);
-
-        // Include the current node
+class Solution {
+public:
+  
+    // Helper function that returns a pair:
+    // first -> max sum when current node is included
+    // second -> max sum when current node is excluded
+    pair<int, int> solve(Node* root) {
+        if (!root) return {0, 0}; // base case: null node
+        
+        auto left = solve(root->left);
+        auto right = solve(root->right);
+        
+        // If we include current node, we cannot include its children
         int include = root->data + left.second + right.second;
-
-        // Exclude the current node
+        
+        // If we exclude current node, we can take max of including or excluding children
         int exclude = max(left.first, left.second) + max(right.first, right.second);
-
+        
         return {include, exclude};
     }
-
-    // Function to return the maximum sum of non-adjacent nodes.
-    int getMaxSum(Node* root) {
-        pair<int, int> result = helper(root);
-        return max(result.first, result.second);
+  
+    // Main function
+    int getMaxSum(Node *root) {
+        auto res = solve(root);
+        return max(res.first, res.second); // return the better of including or excluding root
     }
 };
 
 
 //{ Driver Code Starts.
 
-// Driver code 
-int main()
-{
-  int t;
-  scanf("%d ",&t);
-  while (t--)
-  {
+// Driver code
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
         string s;
-		getline(cin,s);
-		Node* root = buildTree(s);
-		Solution ob;
-        cout<<ob.getMaxSum(root)<<endl;
-  
-cout << "~" << "\n";
-}
-  return 0;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.getMaxSum(root) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
 }
 // } Driver Code Ends
