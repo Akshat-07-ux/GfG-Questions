edc90a3ffd81b67333b78fa21c7716a09d756c2b
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-04-29 19:04:02 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -0,0 +1,135 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+
+// } Driver Code Ends
+
+/*  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};*/
+class Solution {
+public:
+    Node* segregate(Node* head) {
+        if (!head || !head->next) return head;
+
+        // Dummy heads for 0s, 1s, and 2s
+        Node* zeroD = new Node(0);
+        Node* oneD = new Node(0);
+        Node* twoD = new Node(0);
+
+        // Tail pointers for each of the above
+        Node* zero = zeroD;
+        Node* one = oneD;
+        Node* two = twoD;
+
+        Node* curr = head;
+
+        // Traverse and connect nodes into corresponding lists
+        while (curr) {
+            if (curr->data == 0) {
+                zero->next = curr;
+                zero = zero->next;
+            } else if (curr->data == 1) {
+                one->next = curr;
+                one = one->next;
+            } else {
+                two->next = curr;
+                two = two->next;
+            }
+            curr = curr->next;
+        }
+
+        // Connect three sublists: 0s -> 1s -> 2s
+        zero->next = oneD->next ? oneD->next : twoD->next;
+        one->next = twoD->next;
+        two->next = NULL;
+
+        // Head of the new list
+        Node* newHead = zeroD->next;
+
+        // Cleanup dummy nodes
+        delete zeroD;
+        delete oneD;
+        delete twoD;
+
+        return newHead;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver program to test above functions
+int main() {
+    int t, k;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+
+    while (t--) {
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+
+        stringstream ss(input);
+        Node *head = nullptr, *tail = nullptr;
+        int x;
+
+        if (ss >> x) {
+            head = new Node(x);
+            tail = head;
+
+            while (ss >> x) {
+                tail->next = new Node(x);
+                tail = tail->next;
+            }
+
+            // Link the last node to the head if k is 1 to make it circular
+            if (k == 1) {
+                tail->next = head;
+            }
+        }
+
+        Solution ob;
+        Node* newHead = ob.segregate(head);
+        printList(newHead);
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
