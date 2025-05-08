99703bf0682c23894d94d6a4fd4a1ea09c4c7ee6
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-05-08 14:21:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.cpp
+++ Difficulty: Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.cpp
@@ -1,52 +1,27 @@
 //{ Driver Code Starts
-#include <iostream>
-
 #include <bits/stdc++.h>
 using namespace std;
 
-/* Link list Node */
 struct Node {
     int data;
     struct Node* next;
 
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
+    Node(int x)
+        : data(x)
+        , next(nullptr) {}
 };
 
-vector<int> take() {
-    vector<int> arr;
-    string input;
-    getline(cin, input);
-    stringstream ss(input);
-    int number;
-    while (ss >> number) {
-        arr.push_back(number);
+void printList(struct Node* head) {
+    while (head != nullptr) {
+        cout << head->data << ' ';
+        head = head->next;
     }
-    return arr;
-}
-
-Node* inputList(int size, vector<int> v) {
-    if (size == 0)
-        return NULL;
-
-    int val = v[0];
-
-    Node* head = new Node(val);
-    Node* tail = head;
-
-    for (int i = 0; i < size - 1; i++) {
-        val = v[i + 1];
-        tail->next = new Node(val);
-        tail = tail->next;
-    }
-
-    return head;
+    cout << '\n';
 }
 
 
 // } Driver Code Ends
+
 /* Linked List Node
 struct Node {
   int data;
@@ -58,68 +33,111 @@ struct Node {
 }; */
 
 class Solution {
-  public:
-    // Function to find intersection point in Y shaped Linked Lists.
-    int intersectPoint(Node* head1, Node* head2) {
-        if (!head1 || !head2) return -1;
+   public:
+    int getLength(Node* head) {
+        int len = 0;
+        while (head) {
+            len++;
+            head = head->next;
+        }
+        return len;
+    }
 
-        Node* ptr1 = head1;
-        Node* ptr2 = head2;
-
-        // Traverse until the pointers meet or reach the end.
-        while (ptr1 != ptr2) {
-            // Move ptr1 to the next node or to the head of the second list.
-            ptr1 = (ptr1 == nullptr) ? head2 : ptr1->next;
-            // Move ptr2 to the next node or to the head of the first list.
-            ptr2 = (ptr2 == nullptr) ? head1 : ptr2->next;
+    Node* intersectPoint(Node* head1, Node* head2) {
+        int len1 = getLength(head1);
+        int len2 = getLength(head2);
+        
+        // Advance the longer list
+        int diff = abs(len1 - len2);
+        if (len1 > len2) {
+            while (diff--) head1 = head1->next;
+        } else {
+            while (diff--) head2 = head2->next;
         }
 
-        // If they meet at some node, return the data of that node.
-        // Otherwise, they both will be nullptr (end of the lists), return -1.
-        return (ptr1 != nullptr) ? ptr1->data : -1;
+        // Traverse together until we find the intersection or reach NULL
+        while (head1 && head2) {
+            if (head1 == head2) return head1; // Intersection found
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+
+        return NULL; // No intersection
     }
 };
 
 
+
 //{ Driver Code Starts.
 
-/* Driver program to test above function*/
+// Driver program to test the above functions
 int main() {
-    srand(time(0));
-    int T, n1, n2, n3;
-
+    int T;
     cin >> T;
     cin.ignore();
+
     while (T--) {
+        Node *head1 = nullptr, *tail1 = nullptr;
+        Node *head2 = nullptr, *tail2 = nullptr;
+        Node *head3 = nullptr, *tail3 = nullptr;
+        int tmp;
+        string input1, input2, input3;
 
-        vector<int> v1 = take();
-        vector<int> v2 = take();
-        vector<int> v3 = take();
-        int n1 = v1.size(), n2 = v2.size(), n3 = v3.size();
+        getline(cin, input1); // Read the entire line for LinkedList1 elements
+        stringstream ss1(input1);
+        while (ss1 >> tmp) {
+            Node* new_node = new Node(tmp);
+            if (!head1) {
+                head1 = new_node;
+                tail1 = new_node;
+            } else {
+                tail1->next = new_node;
+                tail1 = new_node;
+            }
+        }
 
-        Node* head1 = NULL;
-        Node* head2 = NULL;
-        Node* common = NULL;
+        getline(cin, input2); // Read the entire line for LinkedList2 elements
+        stringstream ss2(input2);
+        while (ss2 >> tmp) {
+            Node* new_node = new Node(tmp);
+            if (!head2) {
+                head2 = new_node;
+                tail2 = new_node;
+            } else {
+                tail2->next = new_node;
+                tail2 = new_node;
+            }
+        }
 
-        head1 = inputList(n1, v1);
-        head2 = inputList(n2, v2);
-        common = inputList(n3, v3);
+        getline(cin, input3); // Read the entire line for common LinkedList elements
+        stringstream ss3(input3);
+        while (ss3 >> tmp) {
+            Node* new_node = new Node(tmp);
+            if (!head3) {
+                head3 = new_node;
+                tail3 = new_node;
+            } else {
+                tail3->next = new_node;
+                tail3 = new_node;
+            }
+        }
 
-        Node* temp = head1;
-        while (temp != NULL && temp->next != NULL)
-            temp = temp->next;
-        if (temp != NULL)
-            temp->next = common;
+        Node* common = head3;
 
-        temp = head2;
-        while (temp != NULL && temp->next != NULL)
-            temp = temp->next;
-        if (temp != NULL)
-            temp->next = common;
+        if (tail1)
+            tail1->next = common; // Connecting LinkedList1 to the common list
+        if (tail2)
+            tail2->next = common; // Connecting LinkedList2 to the common list
+
         Solution ob;
-        cout << ob.intersectPoint(head1, head2) << endl;
-        cout << "~" << endl;
+        Node* intersection = ob.intersectPoint(head1, head2);
+        if (intersection) {
+            cout << intersection->data << endl;
+        } else {
+            cout << "-1" << endl;
+        }
     }
+
     return 0;
 }
 
