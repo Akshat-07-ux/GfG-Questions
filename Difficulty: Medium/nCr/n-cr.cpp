603ab02a8e97b38d770483d5bf7634da232e9b96
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-05-13 17:27:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int nCr(int n, int r) {
+        if (r > n) return 0;
+        if (r == 0 || r == n) return 1;
+        
+        // Use a DP table where dp[i][j] = C(i, j)
+        int dp[n + 1][r + 1];
+
+        // Fill the table using Pascal's Triangle relation:
+        // C(n, r) = C(n-1, r-1) + C(n-1, r)
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= std::min(i, r); j++) {
+                if (j == 0 || j == i)
+                    dp[i][j] = 1;
+                else
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        return dp[n][r];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, r;
+        cin >> n >> r;
+
+        Solution ob;
+        cout << ob.nCr(n, r) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
