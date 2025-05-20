b92e58c7ea83b4eb88ad46f919fd1f42dc473795
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-05-20 14:33:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Burning Tree/burning-tree.cpp
+++ Difficulty: Hard/Burning Tree/burning-tree.cpp
@@ -1,89 +1,6 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    Node *left;
-    Node *right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-
-
-Node *buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node *root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node *> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node *currNode = queue.front();
-        queue.pop();
-
-        // Get the current Node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current Node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current Node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
-//User function Template for C++
-
 /*
-struct Node {
+class Node {
+  public:
     int data;
     Node *left;
     Node *right;
@@ -95,34 +12,35 @@ struct Node {
 };
 */
 class Solution {
-public:
-    Node* bfsToMapParents(Node* root, int target, unordered_map<Node*, Node*>& parentMap) {
+  public:
+    // Function to map each node to its parent and find the target node.
+    void markParents(Node* root, unordered_map<Node*, Node*>& parentTrack, Node* &targetNode, int target) {
         queue<Node*> q;
         q.push(root);
-        Node* targetNode = NULL;
+
         while (!q.empty()) {
-            Node* current = q.front();
-            q.pop();
-            if (current->data == target) {
-                targetNode = current;
-            }
+            Node* current = q.front(); q.pop();
+            if (current->data == target) targetNode = current;
             if (current->left) {
-                parentMap[current->left] = current;
+                parentTrack[current->left] = current;
                 q.push(current->left);
             }
             if (current->right) {
-                parentMap[current->right] = current;
+                parentTrack[current->right] = current;
                 q.push(current->right);
             }
         }
-        return targetNode;
     }
 
     int minTime(Node* root, int target) {
-        unordered_map<Node*, Node*> parentMap;
-        Node* targetNode = bfsToMapParents(root, target, parentMap);
+        unordered_map<Node*, Node*> parentTrack;  // Node to parent map
+        unordered_map<Node*, bool> visited;       // Visited map to prevent re-burning
+        Node* targetNode = nullptr;
 
-        unordered_map<Node*, bool> visited;
+        // Step 1: Fill parent map and locate target
+        markParents(root, parentTrack, targetNode, target);
+
+        // Step 2: BFS from target node
         queue<Node*> q;
         q.push(targetNode);
         visited[targetNode] = true;
@@ -130,64 +48,36 @@ public:
 
         while (!q.empty()) {
             int size = q.size();
-            bool flag = false;
-            for (int i = 0; i < size; i++) {
-                Node* current = q.front();
-                q.pop();
+            bool spread = false;
 
-                // Check left child
+            for (int i = 0; i < size; i++) {
+                Node* current = q.front(); q.pop();
+
+                // Left Child
                 if (current->left && !visited[current->left]) {
-                    flag = true;
+                    spread = true;
                     visited[current->left] = true;
                     q.push(current->left);
                 }
 
-                // Check right child
+                // Right Child
                 if (current->right && !visited[current->right]) {
-                    flag = true;
+                    spread = true;
                     visited[current->right] = true;
                     q.push(current->right);
                 }
 
-                // Check parent node
-                if (parentMap[current] && !visited[parentMap[current]]) {
-                    flag = true;
-                    visited[parentMap[current]] = true;
-                    q.push(parentMap[current]);
+                // Parent
+                if (parentTrack[current] && !visited[parentTrack[current]]) {
+                    spread = true;
+                    visited[parentTrack[current]] = true;
+                    q.push(parentTrack[current]);
                 }
             }
-            if (flag) time++;
+
+            if (spread) time++;  // Only increment time if fire actually spreads
         }
 
         return time;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() 
-{
-    int tc;
-    scanf("%d ", &tc);
-    while (tc--) 
-    {    
-        string treeString;
-        getline(cin, treeString);
-        // cout<<treeString<<"\n";
-        int target;
-        cin>>target;
-        // cout<<target<<"\n";
-
-        Node *root = buildTree(treeString);
-        Solution obj;
-        cout<<obj.minTime(root, target)<<"\n"; 
-
-        cin.ignore();
-
-    }
-
-
-    return 0;
-}
-
-// } Driver Code Ends
